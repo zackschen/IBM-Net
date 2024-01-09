bc384c7ff2eaa8e29c1ee9b3367ecc462655de08
Zacks <cczacks@gmail.com>
2024-01-09 21:13:24 +0800

    modify

---
+++ config/CIFAR100/ib.sh
@@ -0,0 +1,6 @@
+python utils/main.py --model ib \
+--lr 1e-3 --dataset seq-cifar100 --n_epochs 10 --batch_size 256 --csv_log --seed 5 \
+--notes cifar100 \
+--vb_fre 50 \
+--svd \
+--kl_fac 1e-3 \
---
+++ config/TinyImageNet/ib.sh
@@ -0,0 +1,6 @@
+python utils/main.py --model ib \
+--lr 0.001 --dataset seq-tinyimg --n_epochs 300 --batch_size 256 --csv_log --seed 5 \
+--notes tinyimg \
+--vb_fre 50 \
+--kl_fac 1e-3 \
+--svd \
